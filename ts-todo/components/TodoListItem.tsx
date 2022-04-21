fa8efe932e1e4802cece610cc7f1c77a804ff97c
ludie lambright <ludiefletcher@aol.com>
2022-04-21 10:23:24 -0700

    finished todo typescript app

--- ts-todo/components/TodoListItem.tsx
+++ ts-todo/components/TodoListItem.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 interface Props {
     todo: Todo;
+    toggleTodo: ToggleTodo;
 }
 
-export const TodoListItem: React.FC<Props> = ({ todo }) => {
+export const TodoListItem: React.FC<Props> = ({ todo, toggleTodo }) => {
     return (<li>
         <label style={{textDecoration: todo.complete ? 'line-through' : undefined }} >
-        <input type="checkbox" checked={todo.complete} /> {todo.text} </label>
+        <input type="checkbox" checked={todo.complete} onClick={()=> {toggleTodo(todo)}}/>
+         {todo.text} </label>
         </li>);
 };
